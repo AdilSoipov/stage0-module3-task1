c29e1120eca8c7c4c1871e63b56a4da221fc47fe
AdilSoipov <111345038+AdilSoipov@users.noreply.github.com>
2022-08-16 17:39:33 +0500

    Update PrintWordsApp.java

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,8 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        //write your code here
+         System.out.print("java ");
+         System.out.print("is" );
+         System.out.print(" smth" );
     }
 }
